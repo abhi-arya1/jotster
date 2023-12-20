129e5d0d0bdbcda5c88f634ac5efc52b77cd51a7
abhi-arya1 <abhia9167@gmail.com>
2023-12-19 23:24:33 -0800

    need to implement collapse and width resetting to documents (and literally everything else)

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,6 +2,7 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
 
  
 
@@ -26,6 +27,7 @@ const MainLayout = ({
 
     return ( 
         <div className="h-full flex dark:bg-[#1F1F1F]">
+            <Navigation />
             <main className="flex-1 h-full overflow-y-auto">
                 {children}
             </main>
