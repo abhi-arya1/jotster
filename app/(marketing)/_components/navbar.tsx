d7926a9b1fb059705776c4c30a32229fcfed9e4d
abhi-arya1 <abhia9167@gmail.com>
2023-12-19 22:21:07 -0800

    added user login

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,15 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Sign } from "crypto";
+import { Spinner } from "@/components/spinner";
 
 export const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth()
     const scrolled = useScrollTop()
 
     return (
@@ -15,6 +22,33 @@ export const Navbar = () => {
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Log In
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get Jotster
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                    <Button size="sm" asChild>
+                        <Link href="/documents">
+                            Enter Jotster
+                        </Link>
+                    </Button>
+                    <UserButton afterSignOutUrl="/" />
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+/* trunk-ignore-all(eslint/import/no-anonymous-default-export) */
+export default {
+    providers: [
+        {
+            domain: "https://capital-flounder-36.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
