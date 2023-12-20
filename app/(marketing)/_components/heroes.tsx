8b28ae4fc46909978feea7fae93d926d4c72aaac
abhi-arya1 <abhia9167@gmail.com>
2023-12-19 22:54:06 -0800

    fixed spacing bug

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -4,6 +4,7 @@ export const Heroes = () => {
     return (
         <div className="flex flex-col items-center justify-center max-w-5xl">
             <div className="flex items-center">
+
                 <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
                     <Image 
                         src="/documents.png"
@@ -18,6 +19,7 @@ export const Heroes = () => {
                         alt="Documents"
                     />
                 </div>
+
                 <div className="relative h-[400px] w-[400px] hidden md:block">
                     <Image 
                     src="/reading.png"
@@ -32,6 +34,7 @@ export const Heroes = () => {
                     alt="Reading"
                     />
                 </div>
+
             </div>
         </div>
     )
