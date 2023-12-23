5ecb9ed76dc75a5ef077e5fd3e4f14a9557d4fef
abhi-arya1 <abhia9167@gmail.com>
2023-12-22 22:01:44 -0800

    add deletion banner

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
+import { Banner } from "./banner";
 
 interface NavbarProps { 
     isCollapsed: boolean;
@@ -35,6 +36,9 @@ export const Navbar = ({
 
     return ( 
         <>
+            {document.isArchived && (
+                <Banner documentId={document._id} />
+            )}
             <nav className="bg-barkground dark:bg-[1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
                 {isCollapsed && (
                     <MenuIcon role="button" onClick={onResetWidth} className="h-6 w-6 text-muted-foreground" />
