a9e21106d11cff2e334e4a969c18263fca296fa3
abhi-arya1 <abhia9167@gmail.com>
2023-12-22 22:23:17 -0800

    added toolbar skeleton

---
+++ components/toolbar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Doc } from "@/convex/_generated/dataModel"
+
+interface ToolbarProps { 
+    initialData = Doc<"documents">;
+    preview?: boolean; 
+};
+
+export const Toolbar = ({
+    initialData,
+    preview
+}: ToolbarProps) => {
+    return (
+        <div>
+            Toolbar
+        </div>
+    )
+};
