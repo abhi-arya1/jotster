07618d60b8092f88f8550893274896d25ea9cadf
abhi-arya1 <abhia9167@gmail.com>
2023-12-26 19:52:40 -0800

    completed Jotster

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,6 +8,7 @@ import { useParams } from "next/navigation";
 import { Title } from "./title";
 import { Banner } from "./banner";
 import { Menu } from "./menu";
+import { Publish } from "./publish";
 
 interface NavbarProps { 
     isCollapsed: boolean;
@@ -48,6 +49,7 @@ export const Navbar = ({
                     <Title initialData={document} />
                 </div>
                 <div className="flex items-center gap-x-2">
+                    <Publish initialData={document} />
                     <Menu documentId={document._id} />
                 </div>
             </nav>
